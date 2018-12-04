b3d3ed58904ce0b49052ad7ab8c792f2f558d290
Jens <Jens.knutson@outlook.com>
2018-12-04 12:51:05 +0100

    Gjort klart JUnit tester för points

--- src/PointsTest.java
+++ src/PointsTest.java
@@ -1,5 +1,6 @@
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -32,7 +33,6 @@ public class PointsTest {
 		hand.add(new Card(5, Suit.HEARTS));
 		hand.add(new Card(5, Suit.HEARTS));
 		hand.add(new Card(7, Suit.HEARTS));
-		Collections.sort(hand);
 		Points point = new Points(hand);
 		assertEquals(2, point.getPointsVideoPoker());
 	}
@@ -44,8 +44,62 @@ public class PointsTest {
 		hand.add(new Card(2, Suit.HEARTS));
 		hand.add(new Card(5, Suit.HEARTS));
 		hand.add(new Card(7, Suit.HEARTS));
-		Collections.sort(hand);
 		Points point = new Points(hand);
 		assertEquals(3, point.getPointsVideoPoker());
+	}
+	
+	@Test
+	void ladder() {
+		hand.add(new Card(5, Suit.HEARTS));
+		hand.add(new Card(6, Suit.DIAMONDS));
+		hand.add(new Card(7, Suit.HEARTS));
+		hand.add(new Card(8, Suit.HEARTS));
+		hand.add(new Card(9, Suit.HEARTS));
+		Points point = new Points(hand);
+		assertEquals(4, point.getPointsVideoPoker());
+	}
+	
+	@Test
+	void colour() {
+		hand.add(new Card(2, Suit.HEARTS));
+		hand.add(new Card(4, Suit.HEARTS));
+		hand.add(new Card(6, Suit.HEARTS));
+		hand.add(new Card(8, Suit.HEARTS));
+		hand.add(new Card(9, Suit.HEARTS));
+		Points point = new Points(hand);
+		assertEquals(5, point.getPointsVideoPoker());
+	}
+	
+	@Test
+	void fullHouse() {
+		hand.add(new Card(5, Suit.HEARTS));
+		hand.add(new Card(5, Suit.DIAMONDS));
+		hand.add(new Card(5, Suit.SPADES));
+		hand.add(new Card(8, Suit.HEARTS));
+		hand.add(new Card(8, Suit.DIAMONDS));
+		Points point = new Points(hand);
+		assertEquals(6, point.getPointsVideoPoker());
+	}
+	
+	@Test
+	void fours() {
+		hand.add(new Card(5, Suit.HEARTS));
+		hand.add(new Card(5, Suit.DIAMONDS));
+		hand.add(new Card(5, Suit.SPADES));
+		hand.add(new Card(5, Suit.HEARTS));
+		hand.add(new Card(9, Suit.DIAMONDS));
+		Points point = new Points(hand);
+		assertEquals(9, point.getPointsVideoPoker());
+	}
+	
+	@Test
+	void straightFlush() {
+		hand.add(new Card(5, Suit.SPADES));
+		hand.add(new Card(6, Suit.SPADES));
+		hand.add(new Card(7, Suit.SPADES));
+		hand.add(new Card(8, Suit.SPADES));
+		hand.add(new Card(9, Suit.SPADES));
+		Points point = new Points(hand);
+		assertEquals(12, point.getPointsVideoPoker());
 	}
 }
