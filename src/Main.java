4cba42527f57b25a0a7a9b2efbb4b1096c04d037
Jessica <jessica.alingsjo@hotmail.com>
2018-12-05 10:39:52 +0100

    test main med interface

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 
 public class Main {
+	
 	public static void main(String[] args) {
 		Points p = new Points();
 		System.out.println(p.getPointsVideoPoker());
+		
+		//testar interface
+		Interface nf = new Interface();
+		nf.stand();
+		nf.getScore();
+		
+		
 	}
 }
