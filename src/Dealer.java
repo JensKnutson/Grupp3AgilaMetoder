3b790d5d855f66f1519a319c7aaf51d8bc4eb113
Olof Kastrup <olofkastrup@gmail.com>
2018-12-05 09:50:37 +0100

    Initial commit Dealer class

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,4 +1,29 @@
 
-public class Dealer {
+import java.util.*;
 
+public class Dealer {
+	
+	private Deck deck;
+	private List<Card> hand;
+	
+	public Dealer() {
+		deck = new Deck();
+		hand = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			hand.add(deck.draw());
+		}
+		
+	}
+	
+	public List<Card> getHand() {
+		return hand;
+	}
+	
+	public void dealNewCards(List<Card> playerHand) {
+		while (hand.size() < 5) {
+			Card card = deck.draw();
+			hand.add(card);
+		}
+	}
+	
 }
