d1b4b3d57e8da1e2819f84f99ecb6d43b92d365e
Jens <Jens.knutson@outlook.com>
2018-12-07 11:45:14 +0100

    La till en metod i save klassen

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+		
 	}
 }
