4bb237829616f76478445dcbb53bc026c6d37cbf
taydji <taydji01@hotmail.se>
2018-12-10 12:52:25 +0100

    bytt variablerna to Balance och poäng ,U setters and getters

--- src/Bank.java
+++ src/Bank.java
@@ -1,39 +1,35 @@
 import java.io.Serializable;
+/*
+ *Denna klassen innehåller metoder för:
+ * One player should be as the banker
+ * Set the chips " money in the bank"
+ * get chips  form the bank
+ * chips can be returned to the banker to get credit
+ */
 
 public class Bank implements Serializable {
+
+		
+
+	private int balance; 
+	private int poäng;// poäng account
 	
+	public int getBalance() {
+		return balance;
+	}
 
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
 
+	public int getPoäng() {
+		return poäng;
+	}
 
-	/*
-	 *Denna klassen innehåller metoder för:
-	 * One player should be as the banker
-	 * Set the chips " money in the bank"
-	 * get chips  form the bank
-	 * chips can be returned to the banker to get credit
-	 */
-
-
-		public int bank;
-
-		public Bank() { // méthode banque pour prendre les valeur banque
-			bank = 0;
-		}
-
-		public Bank(int indata) { 
-			this.bank = indata;
-		}
-		public int getBank() {
-			return bank;
-		}
-
-		public void setBank(int setdata) {
-			bank = setdata;
-		}
-
-		public void Bankroll(int chips) {
-			bank += setchips;
-		}
+	public void setPoäng(int poäng) {
+		this.poäng = poäng;
+	}
+	
 
 	}
 
