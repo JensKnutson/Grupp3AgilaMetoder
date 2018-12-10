a7d65343e9eafab577c3d1dd52e87877d627a5ed
Jens <Jens.knutson@outlook.com>
2018-12-10 10:33:07 +0100

    Ändringar i interface och points

--- src/Interface.java
+++ src/Interface.java
@@ -11,11 +11,11 @@ public class Interface {
 	Interface(Dealer deal) {
 		this.deal = deal;
 	}
+	
 	// skrivet ut spelarens hand
 	public void stand() {
 
-		Dealer d = new Dealer();
-		cardInterface = d.getHand(); // hämta från dealer
+		cardInterface = deal.getHand(); // hämta från dealer
 
 		// skriver ut de dragna kortet.
 		for (int i = 0; i < cardInterface.size(); i++) {
@@ -35,54 +35,52 @@ public class Interface {
 	} */
 
 	//poäng.
+	
 	public void getScore() {
-		Points p = new Points();
-		Dealer d = new Dealer();
-		cardInterface = d.getHand();
-		
-         for (int i = 0; i< cardInterface.size();i++ ) {
-		System.out.println("Points: " + p.getPointsVideoPoker());
-         }
+		Points p = new Points(deal.getHand());
+       System.out.println("Points: " + p.getPointsVideoPoker());
+        
 	}
 
 
 	// metod förkortning av suit.
-	public String getSuit() {
-		// test värden
-		Card card = new Card(13, Suit.SPADES);
-
-		String H = "H";
-		String S = "S";
-		String D = "D";
-		String C = "C";
-		String n = "";
-
-		if (card.getSuit().equals(Suit.HEARTS)) {
-
-			return H;
-		}
-
-		else if (card.getSuit().equals(Suit.SPADES)) {
-			return S;
-		}
-
-		else if (card.getSuit().equals(Suit.DIAMONDS))
-
-		{
-			return D;
-		} else if (card.getSuit().equals(Suit.CLUBS)) {
-			return C;
-		} else
-
-			return n;
-
-	}
+//	public String getSuit() {
+//		// test värden
+//		Card card = new Card(13, Suit.SPADES);
+//		
+//
+//		String H = "H";
+//		String S = "S";
+//		String D = "D";
+//		String C = "C";
+//		String n = "";
+//
+//		if (card.getSuit().equals(Suit.HEARTS)) {
+//
+//			return H;
+//		}
+//
+//		else if (card.getSuit().equals(Suit.SPADES)) {
+//			return S;
+//		}
+//
+//		else if (card.getSuit().equals(Suit.DIAMONDS))
+//
+//		{
+//			return D;
+//		} else if (card.getSuit().equals(Suit.CLUBS)) {
+//			return C;
+//		} else
+//
+//			return n;
+//
+//	}
 
 	// metod valör på korten
-	public String getCard() {
+	public String getCard(Card card) {
 
 		// test värden.
-		Card card = new Card(2, Suit.SPADES);
+		//		Card card = new Card(2, Suit.SPADES);
 
 		if (card.getValue() == 11) {
 			return "J";
@@ -102,50 +100,50 @@ public class Interface {
 	}
 
 	// metod siffra kort
-	public static int getCardValue() {
-		// test värden
-		Card card = new Card(1, Suit.SPADES);
-		int in = 0;
-
-		if (card.getValue() == 2) {
-
-			return 2;
-		} else if (card.getValue() == 3) {
-
-			return 3;
-		} else if (card.getValue() == 4) {
-
-			return 4;
-		} else if (card.getValue() == 5) {
-
-			return 5;
-		} else if (card.getValue() == 6) {
-
-			return 6;
-		} else if (card.getValue() == 7) {
-
-			return 7;
-		} else if (card.getValue() == 8) {
-
-			return 8;
-		} else if (card.getValue() == 9) {
-
-			return 9;
-		} else
-
-			return in;
-
-	}
+//	public int getCardValue() {
+//		// test värden
+//		Card card = new Card(1, Suit.SPADES);
+//		int in = 0;
+//
+//		if (card.getValue() == 2) {
+//
+//			return 2;
+//		} else if (card.getValue() == 3) {
+//
+//			return 3;
+//		} else if (card.getValue() == 4) {
+//
+//			return 4;
+//		} else if (card.getValue() == 5) {
+//
+//			return 5;
+//		} else if (card.getValue() == 6) {
+//
+//			return 6;
+//		} else if (card.getValue() == 7) {
+//
+//			return 7;
+//		} else if (card.getValue() == 8) {
+//
+//			return 8;
+//		} else if (card.getValue() == 9) {
+//
+//			return 9;
+//		} else
+//
+//			return in;
+//
+//	}
 
 	// skriver ut
-	public String getCardPrint() {
+	public String getCardPrint(Card card) {
 		// ett värde som konstruktorn vill ha.
-		Card card = new Card(2, Suit.CLUBS);
+//		Card card = new Card(2, Suit.CLUBS);
 
 		if (card.getValue() <= 10 && card.getValue() > 2) {
-			return getSuit() + " " + getCardValue() + ",";
+			return card.getSuit() + " " + card.getValue() + ",";
 		} else
-			return getSuit() + " " + getCard() + ",";
+			return card.getSuit() + " " + card.getValue() + ",";
 
 	}
 
