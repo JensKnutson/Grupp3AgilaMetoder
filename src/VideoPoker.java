f689c67b9a6feb5ba0a5184916f9ff0642b8ae08
Reava <mikel.wassenius@gmail.com>
2018-12-10 11:18:32 +0100

    Fixa spelmenyer

--- src/VideoPoker.java
+++ src/VideoPoker.java
@@ -5,20 +5,48 @@ import java.util.Scanner;
 
 public class VideoPoker {
 	
+	Scanner scVP = new Scanner(System.in);
+	Dealer deal;
+	Interface intf;
 	
-	Dealer deal = new Dealer();
-	Interface intf = new Interface(deal);
+	public VideoPoker() {
+		vpMainMenu();
+	}
 	
+	public void vpMainMenu() {
+		int playerInput;
+		do {
+			deal = new Dealer();
+			intf = new Interface(deal);
+			System.out.println("---Video Poker---");
+			System.out.println("Välj:\n" + "1 - Ny hand\n" + "2 - Ladda spel\n" + "0 - Avsluta spel");
+			playerInput = scVP.nextInt();
+			switch (playerInput) {
+			case 1:
+				vpGameMenu();
+				break;
+			case 2:
+				//Ladda spel
+				break;
+			case 0:
+				break;
+
+			default:
+				System.out.println("Felaktig inmatning");
+				break;
+			}
+			
+		} while (playerInput != 0);
+	}
 	
 //	Spelmenyn: - anpassad för att handmodifikation skall ske i Dealer
-	public void vpMenu() {
+	public void vpGameMenu() {
 		int playerInput;
-		Scanner scVP = new Scanner(System.in);
 		do {
-			playerInput = scVP.nextInt();
 			System.out.println("Välj vilket kort du vill byta");
-			System.out.printf("LÄGG IN FORMATTERING!", 1, 2, 3, 4, 5, "0: Byten klart");		//Visuellt vilket kort/alternativ som är kopplat till vilken inmatning
-			intf.getHand();					//INTERFACE -Metod finns inte ännu, (skriv ut nuvarande hand)
+			System.out.println("Välj vilket kort du vill byta. " + "0: Byten klart");		//Visuellt vilket kort/alternativ som är kopplat till vilken inmatning
+			intf.stand();					//INTERFACE -Metod finns inte ännu, (skriv ut nuvarande hand)
+			playerInput = scVP.nextInt();
 			switch (playerInput) {
 			case 1:
 				deal.discardCard(0);		
@@ -36,7 +64,6 @@ public class VideoPoker {
 				deal.discardCard(4);		
 				break;
 			case 0:
-				scVP.close();
 				break;
 
 			default:
@@ -46,8 +73,10 @@ public class VideoPoker {
 		} while (playerInput != 0);
 		
 		deal.redrawHand();					
-		intf.getHand();					//INTERFACE -Metod finns inte ännu, (skriv ut nuvarande hand)
-		intf.getScore();				
+		intf.stand();					//INTERFACE -Metod finns inte ännu, (skriv ut nuvarande hand)
+		intf.getScore();
+		
+		
 		
 
 		
