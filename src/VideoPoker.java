ecc3b951bc5bd57c3a564942f43c68f86fc06449
Reava <mikel.wassenius@gmail.com>
2018-12-10 10:23:55 +0100

    Rensa kod

--- src/VideoPoker.java
+++ src/VideoPoker.java
@@ -2,13 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import jdk.nashorn.internal.ir.WhileNode;
 
-//	Mikel
 public class VideoPoker {
-//	Sortera efter färg/värde? -INTERFACE / DEALER
 	
-	//private List<Card> playerHand = new ArrayList<>(); //FLYTTA ALL handmodifikation till dealer??
+	
 	Dealer deal = new Dealer();
 	Interface intf = new Interface();
 	
@@ -24,19 +21,19 @@ public class VideoPoker {
 			intf.getHand();					//INTERFACE -Metod finns inte ännu, (skriv ut nuvarande hand)
 			switch (playerInput) {
 			case 1:
-				deal.discardCard(0);		//Metod finns inte ännu
+				deal.discardCard(0);		
 				break;
 			case 2:
-				deal.discardCard(1);		//Metod finns inte ännu
+				deal.discardCard(1);		
 				break;
 			case 3:
-				deal.discardCard(2);		//Metod finns inte ännu
+				deal.discardCard(2);		
 				break;
 			case 4:
-				deal.discardCard(3);		//Metod finns inte ännu
+				deal.discardCard(3);		
 				break;
 			case 5:
-				deal.discardCard(4);		//Metod finns inte ännu
+				deal.discardCard(4);		
 				break;
 			case 0:
 				scVP.close();
@@ -48,38 +45,13 @@ public class VideoPoker {
 			}
 		} while (playerInput != 0);
 		
-		deal.redrawHand();					//DEALER -Metod finns inte ännu, (dra nya kort för varje bytt kort)
+		deal.redrawHand();					
 		intf.getHand();					//INTERFACE -Metod finns inte ännu, (skriv ut nuvarande hand)
-		intf.getScore();				//INTERFACE -Metod finns inte ännu, (skriv ut Score, kanske lagra score, alt skriv ut vinst i pengar?
+		intf.getScore();				
 		
-//		Kommande problem: Fortsätta spela samma spel(/session) -> Alternativ här eller gjort genom main?
-//						  Hur byta till spela för pengar?
+
 		
 	}
 	
-//	Metod för hämta hand från dealern
-//	public void fetchHand() {
-//		playerHand = deal.getHand();	//DEALER-metod
-//	}
-	
-//	Metod för slänga kort    FLYTTA TILL DEALER! ->    lägg till byta kort - skicka till dealer - ta emot hand+nya kort från dealer
-//	public void discardCard(int index) {
-//		playerHand.remove(index);
-//	}
-	
-//	Metod för att skicka in modifierad hand i Dealer och få nya kort för varje kasserad
-//	public void newCards() {
-//		playerHand = deal.dealNewCards(playerHand);		//DEALER-metod
-//	}
-	
-//	Metod för stand/get score -----Tänk om, denna är onödig -"Löst" i spelmenyn
-//	public void stand() {
-//		intf.getScore(playerHand);		//INTERFACE-metod
-//	}
-	
-//	Getter för playerHand
-//	public List<Card> getPlayerHand() {
-//		return playerHand;
-//	}
 	
 }
